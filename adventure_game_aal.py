b04d234ebf78c5c24f6fa65abffd64acc95b3cf7
adrianlievano <32432374+adrianlievano@users.noreply.github.com>
2019-02-03 23:13:56 -0800

    Adrian Lievano first submission to Udacity reviewers

--- adventure_game_aal.py
+++ adventure_game_aal.py
@@ -1,12 +1,14 @@
 import time
 import random
 
-#This function prints messages to the user
+
+# This function prints messages to the user
 def print_pause(message_to_print):
     print(message_to_print)
     time.sleep(1)
 
-#This function checks if the user has the key item to win
+
+# This function checks if the user has the key item to win
 def item_check(items):
     flag = 0
     if ("Sword of Ogoroth" in items):
@@ -15,7 +17,8 @@ def item_check(items):
     else:
         return flag
 
-#This function stops or restarts the game
+
+# This function stops or restarts the game
 def restart_game():
     restart_var = input("Would you like to play again? (y/n)")
     if (restart_var == 'y'):
@@ -25,20 +28,20 @@ def restart_game():
     else:
         print_pause("Thanks for playing! See you next time.")
 
-#This function checks if the user will knock of peer into the cave throughout the game
+
+# This function checks if the user will knock of peer into the cave throughout
+# the game.
 def knock_or_peer_check(items, animal):
-    ##Code to refactor
     print_pause(" ")
     print_pause("Enter 1 to knock on the door of the house")
     print_pause("Enter 2 to peer into the cave.")
     print_pause("What would you like to do?")
     reply = input("(Please enter 1 or 2.)\n")
     flag = item_check(items)
-    #if user peers into cave again and has the sword
-    #while (reply != '1' or reply != '2'):
-    while (reply != '1' or reply != '2'):
+
+    while (reply is not '1' or '2'):
         reply = input("Please enter 1 or 2.\n")
-        if (reply =='1' or reply == '2'):
+        if (reply == '1' or reply == '2'):
             break
 
     if (reply == '2' and flag == 0):
@@ -46,15 +49,16 @@ def knock_or_peer_check(items, animal):
         print_pause("It turns out to be only a very small cave.")
         print_pause("Your eye catches a glint of metal behind a rock.")
         print_pause("You have found the magical Sword of Ogoroth!")
-        print_pause("You discard your silly old dagger and take the sword with you.")
+        print_pause("You discard your silly old dagger and take the sword with"
+                    "you.")
         print_pause("You walk back out to the field.")
         items.append("Sword of Ogoroth")
-        print(items)
-        knock_or_peer_check(items, animal) ##
+        knock_or_peer_check(items, animal)
 
     elif (reply == '2' and flag == 1):
         print_pause("You peer cautiously into the cave.")
-        print_pause("You've been here before, and gotten all the good stuff. It's just an empty cave now.")
+        print_pause("You've been here before, and gotten all the good stuff."
+                    "It's just an empty cave now.")
         print_pause("You walk back out to the field.")
         knock_or_peer_check(items, animal)
 
@@ -64,40 +68,38 @@ def knock_or_peer_check(items, animal):
     elif (reply == '1' and flag == 0):
         fight_or_run_check(animal, reply, items)
 
-    #elif (reply != '1' or reply != '2'):
-    #        reply = input("(Please enter 1 or 2.)\n")
-    #        print('HERE WE ARE')
-    #elif (reply != '1' or reply != '2'):
-        #reply = input("Please enter 1 or 2.)\n")
-        #print('here we are')
-        #knock_or_peer_check(items, animal)
-
     return reply, items
 
+
+# This function checks if the user is running or fighting
 def fight_or_run_check(animal, reply, items):
     print_pause("You approach the door of the house.")
-    print_pause("You are about to knock when the door opens and out steps a " + animal)
+    print_pause("You are about to knock when the door opens and out steps a " +
+                animal)
     print_pause("Eep! This is the " + animal + "'s" + " house!")
-    print_pause("The " + animal +  " attacks you!")
+    print_pause("The " + animal + " attacks you!")
     flag = item_check(items)
-    reply = input("Would you like to (1) fight or (2) run away?")
+    reply = input("Would you like to (1) fight or (2) run away?\n")
 
-    while (reply != '1' or reply != '2'):
+    while (reply is not '1' or '2'):
         reply = input("Please enter 1 or 2.\n")
-        if (reply =='1' or reply == '2'):
+        if (reply == '1' or reply == '2'):
             break
 
-    print(flag)
-
     if (reply == '1' and flag == 1):
-        print_pause("As the wicked " + animal + " moves to attack, you unsheath your new sword.")
-        print_pause("The Sword of Ogoroth shines brightly in your hand as you brace yourself for the attack.")
-        print_pause("But the wicked " + animal + " takes one look at your shiny new toy and runs away!")
-        print_pause("You have rid the town of the wicked " + animal + " You are victorious!")
+        print_pause("As the wicked " + animal + " moves to attack, you unsheat"
+                    "h your new sword.")
+        print_pause("The Sword of Ogoroth shines brightly in your hand as you"
+                    "brace yourself for the attack.")
+        print_pause("But the wicked " + animal + " takes one look at your shin"
+                    "y new toy and runs away!")
+        print_pause("You have rid the town of the wicked " + animal + " You ar"
+                    "e victorious!")
         restart_game()
 
     elif ((reply == '1' and flag == 0)):
-        print_pause("You feel a bit under-prepared for this, what with only having a tiny dagger.")
+        print_pause("You feel a bit under-prepared for this, what with only""ha
+                    ving a tiny dagger.")
         reply = input("Would you like to (1) fight or (2) run away?")
 
         if (reply == '1'):
@@ -107,34 +109,30 @@ def fight_or_run_check(animal, reply, items):
             restart_game()
 
         elif (reply == '2'):
-            print_pause("You run back into the field. Luckily, you don't seem to have been followed.")
+            print_pause("You run back into the field. Luckily, you don't seem""
+                        to have been followed.")
             knock_or_peer_check(items, animal)
 
     elif (reply == '2' and flag == 0):
-            print_pause("You run back into the field. Luckily, you don't seem to have been followed.")
+            print_pause("You run back into the field. Luckily, you don't seem""
+                        to have been followed.")
             knock_or_peer_check(items, animal)
 
     elif (reply == '2' and flag == 1):
-            print_pause("You run back into the field. Luckily, you don't seem to have been followed.")
+            print_pause("You run back into the field. Luckily, you don't seem""
+                        to have been followed.")
             knock_or_peer_check(items, animal)
 
-    #error proofing
-    ##elif (reply != '1' or reply != '2'):
-    #    reply = input("Please enter 1 or 2.\n")
-    #    print('here we are')
-        #knock_or_peer_check(items, animal)
 
-
-
-
-
-#This function runs the adventure game from start to finish
+# This function runs the adventure game from start to finish
 def play_game():
     items = []
     animal_bases = ["dragon", "fairie", "gorgon", "pirate"]
     animal = random.choice(animal_bases)
-    print_pause("You find yourself standing in an open field, filled with grass and yellow wildflowers.")
-    print_pause("Rumor has it that a " + animal + " is somewhere around here, and has been terrifying the nearby village." )
+    print_pause("You find yourself standing in an open field, filled with gras"
+                "s and yellow wildflowers.")
+    print_pause("Rumor has it that a " + animal + " is somewhere around here,"
+                "and has been terrifying the nearby village.")
     print_pause("In front of you is a dark house.")
     print_pause("To your right is a dark cave.")
     items.append('dagger')
